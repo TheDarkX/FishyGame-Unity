56954b52574561f75d018a3e9d8a385b54ca6a7f
TheDarkX <nokiaking0000@gmail.com>
2018-04-16 13:31:33 +0800

    Ready to build
    finished tutorial up to #9
    https://unity3d.college/2016/01/25/intro-to-unity3d-building-flappy-bird-part-9/

---
+++ Assets/Code/Fish.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fish : MonoBehaviour {
+
+    [SerializeField]
+    private float _upwardForceMultipler = 100f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*
+         Fire1 is a default button in Unity3D that is mapped to many things 
+         [Space, Left-Ctrl, Left Click, A button (Xbox), X button (PS), etc].
+        */
+        bool pressedFireButton = Input.GetButton("Fire1"); 
+        if (pressedFireButton)
+        {
+            Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+            // Vector3.zero is just a shortcut to get a Vector3 with 0 for all 3 of it’s values (X, Y, Z)
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.AddForce(Vector3.up * _upwardForceMultipler);
+        }
+
+        if(transform.position.y > 6f || transform.position.y < -6f)
+        {
+            //Application.LoadLevel(0);
+            SceneManager.LoadScene(0);
+        }
+	}
+}
---
+++ Assets/Code/ScoreKeeper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour {
+
+    private int _currentScore = 0;
+
+	public void IncrementScore()
+    {
+        _currentScore++;
+        Text scoreText = GetComponent<Text>();
+        scoreText.text = "Score: " + _currentScore.ToString();
+    }
+	
+	
+}
